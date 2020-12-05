6f64d7bf6e755b706cab6640dc9d59e8f8700071
EvgenTen <30858011+EvgenTen@users.noreply.github.com>
2020-12-05 10:39:33 +0200

    1st

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(5, 0);
+    private Vector2 movement;
+    public Vector2 direction = new Vector2(-1, 0);
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(speed.x * direction.x, 0);
+    }
+
+     void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = movement;   
+    }
+}
